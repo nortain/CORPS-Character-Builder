811e83e87314d3e8468392338f7718e3b9886d4b
Mike McEachron <mike.mceachron@eyefinity.com>
2017-10-25 18:25:48 -0700

    and so it begins...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+
+import {Race} from "./shared/character/race/race";
+import {Action, Reducer, Store} from "ngrx/@ngrx/store";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  counter$: Observable<number>;
+
+  constructor(private store: Store<number>) {
+    this.counter$ = this.store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch({type: 'INCREMENT'});
+  }
+
+  decrement() {
+    this.store.dispatch({type: 'DECREMENT'});
+  }
 }
+
+export const counter: Reducer<number> = (state: number = 0, action: Action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
