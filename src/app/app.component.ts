3aa56af59e7fc047ce07f306628d8b5f804c359e
Mike McEachron <mike.mceachron@eyefinity.com>
2017-11-05 09:56:30 -0800

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,5 @@
 import {Component} from '@angular/core';
-import {Observable} from "rxjs/Rx";
 
-
-import {Store} from "ngrx/@ngrx/store";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
-
-
-type Reducer<T> = (state: T, action: Action) => T;
-
-export interface Action {
-  type: string;
-  payload?: any;
-}
-
-export function counter(state: number = 0, action: Action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-export class store extends BehaviorSubject<any> {
-  constructor(initalState: any) {
-    super(initalState);
-  }
-}
 
 
 @Component({
@@ -37,19 +8,6 @@ export class store extends BehaviorSubject<any> {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  counter$: Observable<number>;
-
-  constructor(private store: Store<number>) {
-    this.counter$ = this.store.select('counter');
-  }
-
-  increment() {
-    this.store.dispatch({type: 'INCREMENT'});
-  }
-
-  decrement() {
-    this.store.dispatch({type: 'DECREMENT'});
-  }
 
 }
 
