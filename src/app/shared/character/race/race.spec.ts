944138b4778916ca7c032790e25b638c11fd97fb
Mike McEachron <nortain@hotmail.com>
2018-01-09 00:26:10 -0800

    Finished tests for the race class

--- src/app/shared/character/race/race.spec.ts
+++ src/app/shared/character/race/race.spec.ts
@@ -2,8 +2,10 @@ import {Race} from "./race";
 import {RaceType} from "./race-type.enum";
 import {VisionType} from "./vision-type.enum";
 import {MagicDefenseType} from "../../magic-defense/magic-defense-type.enum";
-import {NON_HUMAN_AVAILABLE_ATTRIBUTE_POINTS, STARTING_PLAYER_RACES} from "../../constants";
+import {NON_HUMAN_AVAILABLE_ATTRIBUTE_POINTS} from "../../constants";
 import {RacialSubType} from "./racial-sub-type.enum";
+import {ThemeType} from "../../theme-points/theme-type.enum";
+import {AttributeName} from "../../attribute/attribute-name.enum";
 
 describe('Race', () => {
   let elf, human, dwarf, prim, fey, halfling, orc, elder;
@@ -84,13 +86,46 @@ describe('Race', () => {
     expect(fey.formatText(fey.activeBonuses[0].value)).toBe("As a minor action you gain advantage to the first roll of an attack or spell cast this turn.  You also increase the damage inflicted to all targets by 1 extra damage die.");
   });
 
-  it('should be able to format text for a primental\'s elemental resistance', () => {
+  it('should be able to format text for an air primental\'s elemental resistance', () => {
     prim.initializeData(prim.raceType, 1, RacialSubType.Air);
     expect(prim.formatText(prim.passiveBonuses[0].value)).toBe("You gain 3 resistance to the Lightning keyword and 2 to all other magic damage keywords");
   });
 
-  it('should be able to format test for a primental\'s active elemental release', () => {
+  it('should be able to format test for a fire primental\'s active elemental release', () => {
     prim.initializeData(prim.raceType, 1, RacialSubType.Fire);
     expect(prim.formatText(prim.activeBonuses[0].value)).toBe("As a minor action any successful attacks gain the heat keyword and do an additional 1d8+3 heat damage (roll once).");
+  });
+
+  it('should be able to format the text for the earth elementals active elemental power', () => {
+    prim.initializeData(prim.raceType, 6, RacialSubType.Earth);
+    expect(prim.formatText(prim.activeBonuses[0].value)).toBe("As a minor action strike the ground with tremendous force.  All adjacent enemies must make a hard saving throw (17) throw or be knocked prone.");
+  });
+
+  it('should be able to format text for the water elementals passive power', function () {
+    prim.initializeData(prim.raceType, 3, RacialSubType.Water);
+    expect(prim.formatText(prim.activeBonuses[0].value)).toBe("As a swift action you ignore all difficult terrain and your movement does not provoke opportunity attacks until the end of your turn.");
+  });
+
+  it('should be able to format text for the human active power', function () {
+    human = new Race(RaceType.Human, 7);
+    expect(human.formatText(human.activeBonuses[0].name)).toBe("Oh the Humanity");
+    expect(human.formatText(human.activeBonuses[0].value)).toBe("As a minor action you can gain a +4 to hit with all attacks and any critical rolls gain 1 additional die until the end of your turn.");
+  });
+
+  it('should be able to get the talent bonus for a given race', function () {
+    expect(human.talentBonus[0]).toBe(ThemeType.General);
+    expect(elf.talentBonus[0]).toBe(ThemeType.Stealth);
+    expect(elder.talentBonus.length).toBe(2);
+    expect(orc.talentBonus[0]).toBe(ThemeType.Combat);
+    expect(prim.talentBonus[0]).toBe(ThemeType.Magic);
+  });
+
+  it('should be able to determine the starting attributes for a given race', function () {
+    expect(dwarf.startingAttributes).toContain(AttributeName.Vitality);
+    expect(dwarf.startingAttributes).toContain(AttributeName.Reasoning);
+    expect(dwarf.startingAttributes).toContain(AttributeName.Brawn);
+    expect(dwarf.availableAttributePoints).toBe(4);
+    expect(human.startingAttributes).toBeUndefined();
+    expect(human.availableAttributePoints).toBe(6);
   });
 });
