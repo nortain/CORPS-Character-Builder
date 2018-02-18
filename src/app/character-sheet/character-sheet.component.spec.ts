034cfc63b53bd90bc9cad85a3b4c33387c8d3d16
Mike McEachron <nortain@hotmail.com>
2018-02-18 11:14:20 -0800

    Fixed tests, need to add more UI components to the character sheet. TODO add more tests for race and dropdown to affect character correctly.

--- src/app/character-sheet/character-sheet.component.spec.ts
+++ src/app/character-sheet/character-sheet.component.spec.ts
@@ -7,6 +7,8 @@ import {mockCharacter} from "../shared/constants/testing-constants";
 import {RaceType} from "../shared/character/race/race-type.enum";
 import {Level} from "../shared/character/level.enum";
 import {Character} from "../shared/character/character";
+import {By} from "@angular/platform-browser";
+import {InputComponent} from "../shared/ui/input/input.component";
 
 describe('CharacterSheetComponent', () => {
   let component: CharacterSheetComponent;
@@ -32,9 +34,19 @@ describe('CharacterSheetComponent', () => {
   });
 
   it('should be able to reload a character', () => {
+    component.character.name = "Bob";
     expect(component.character).toEqual(mockCharacter());
     component.reloadCharacter(RaceType.Burman, Level.Two);
-    expect(component.character).not.toEqual(mockCharacter);
+    expect(component.character).not.toEqual(mockCharacter());
     expect(component.character).toEqual(new Character("Bob", RaceType.Burman, Level.Two));
+  });
+
+  it('should be able to set character name via the input component', function () {
+    expect(component.character.name).toBe("");
+    const nameInput = fixture.debugElement.query(By.css("input#NameId")).nativeElement;
+    nameInput.value = "Bob";
+    nameInput.dispatchEvent(new Event("input"));
+    fixture.detectChanges();
+    expect(component.character.name).toBe("Bob");
   });
 });
--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Character} from "../shared/character/character";
-import {Race} from "../shared/character/race/race";
 import {AttributeService} from "../shared/attribute/attribute.service";
 import {RaceType} from "../shared/character/race/race-type.enum";
 import {DropdownValueObject} from "../shared/ui/dropdown/dropdown-value-object";
 import {Level} from "../shared/character/level.enum";
+import {InputComponent} from "../shared/ui/input/input.component";
 
 @Component({
   selector: 'corps-character-sheet',
@@ -24,11 +24,11 @@ export class CharacterSheetComponent implements OnInit {
     this.races = this.attributeService.getArrayOfDropdownValueObjectsFromEnum(RaceType, false);
     this.levels = this.attributeService.getArrayOfDropdownValueObjectsFromEnum(Level, false);
     const raceType = this.races[0].label;
-    this.character = new Character("Bob", RaceType[raceType] as RaceType);
+    this.character = new Character("", RaceType[raceType] as RaceType);
   }
 
   reloadCharacter(raceType: RaceType, level: Level) {
-
+    this.character = new Character(this.character.name, raceType, level);
   }
 
 }
